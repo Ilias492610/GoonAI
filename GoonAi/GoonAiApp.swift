ca8033560b739b46e10ea3cd368e1ec400e0f633
Ilias <hamdaoui.ilias14@gmail.com>
2025-11-19 13:13:22 +0100

    Initial Commit

---
+++ GoonAi/GoonAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoonAiApp.swift
+//  GoonAi
+//
+//  Created by ilias hamdaoui on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GoonAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
